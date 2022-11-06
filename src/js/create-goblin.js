95699cfeabb00d438a75a7248f67fe6f621b6def
Vladislav <gto1103@gmail.com>
2022-11-07 01:04:15 +0300

    Ok

--- src/js/create-goblin.js
+++ src/js/create-goblin.js
@@ -1,6 +1,5 @@
 export default function createGoblin() {
   const image = document.createElement('img');
-  image.src = './img/goblin.png';
   image.className = 'goblin';
   return image;
 }
--- src/js/game.js
+++ src/js/game.js
@@ -1,41 +1,29 @@
 import createGoblin from './create-goblin';
 
-const cells = [...document.querySelectorAll('.cell')];
-// let hitCounter = document.getElementById("hit-counter");
+const cells = document.querySelectorAll('.cell');
 let lastCell = null;
-// let timeUp = false;
-// let hits = 0;
 
 function removeEnemy() {
   if (lastCell === null) {
     return;
   }
-  cells[lastCell].firstChild.remove();
+  lastCell.firstChild.remove();
 }
 
-function showEnemy(index) {
+function showEnemy(cell) {
   const enemy = createGoblin();
-  index.appendChild(enemy);
+  cell.appendChild(enemy);
 }
 
-function randomChangeEnemy() {
+export default function randomChangeEnemy() {
   const index = Math.floor(Math.random() * cells.length);
   const cell = cells[index];
   if (cell === lastCell) {
-    return randomChangeEnemy();
+    return;
   }
-
   removeEnemy();
   lastCell = cell;
   showEnemy(lastCell);
-  return cell;
 }
 
-function startGame() {
-  // hitCounter.textContent = '0';
-  //  timeUp = false;
-  //  hits = 0;
-  setTimeout(randomChangeEnemy(cells), 2000);
-}
-
-startGame();
+setInterval(randomChangeEnemy, 2000);
